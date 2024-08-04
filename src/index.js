25ffb866ce5155c4501f393c287dd567399a9de7
zainab721 <155131588+zainab721@users.noreply.github.com>
2024-08-04 14:18:49 -0500

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+
   let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
@@ -15,8 +16,36 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
+
+function formatDate(date)  {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
@@ -49,34 +78,7 @@ getForecast(response.data.city);
   function displayForecast(response)  {
   
   }
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-
-
-let formattedDay = days[day];
-return `${formattedDay} ${hours}:${minutes}`;
-}
-
+  
 function searchCity(city) {
   let apiKey = "e095dc345c934o5ae8fb54ctcbb40bf0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
